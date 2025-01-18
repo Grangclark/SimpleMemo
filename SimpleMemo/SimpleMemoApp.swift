0c5ca914ca23eebd039bc942b3008d133238ee8f
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-18 22:15:32 +0900

    Initial Commit

---
+++ SimpleMemo/SimpleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMemoApp.swift
+//  SimpleMemo
+//
+//  Created by 長橋和敏 on 2025/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
